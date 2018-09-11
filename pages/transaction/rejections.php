8619b335df70f77d05990ee055eacf8fb4f4d285
josua.obando <josua.obando@gmail.com>
2018-09-10 21:41:32 -0600

    add declined transactions

--- pages/transaction/rejections.php
+++ pages/transaction/rejections.php
@@ -93,7 +93,6 @@ try{
       if($transactions && count($transactions) > 0){
       ?>
       <div class="panel panel-default">
-        <div class="panel-heading">Error</div>
         <div class="panel-body">
           <div class="table-responsive">
             <table class="table table-striped table-bordered table-hover wrap-table">
