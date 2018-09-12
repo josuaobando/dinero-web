c3a8068e6c8352af1f6cc66c2df2acd6515ad968
josuaobando <Josh2909>
2018-09-12 13:22:00 -0600

    order columns

--- pages/transaction/rejections.php
+++ pages/transaction/rejections.php
@@ -38,17 +38,17 @@ try{
             <table class="table table-striped table-bordered table-hover wrap-table">
               <thead>
               <tr>
-                <th>Agency</th>
-                <th>Type</th>
                 <th>Timestamp</th>
                 <th>Timespan</th>
+                <th>Type</th>
+                <th>Agency</th>
                 <th>Amount</th>
                 <th>Account</th>
                 <th>Customer</th>
-                <th>Error</th>
-                <th>Message</th>
                 <th>Id</th>
                 <th>Control Number</th>
+                <th>Error</th>
+                <th>Message</th>
               </tr>
               </thead>
               <tbody>
@@ -76,17 +76,17 @@ try{
                 }
 
                 echo "<tr class='$rowType'>
-														<td>$agencyType</td>
-														<td>$requestType</td>
 														<td>$timeStamp</td>
 														<td>$timeSpan</td>
+														<td>$requestType</td>
+														<td>$agencyType</td>
 														<td>$$amount</td>
 														<td>$username</td>
 														<td>$customer</td>
-														<td>$error</td>
-														<td>$message</td>
 														<td>$transactionId</td>
 														<td>$controlNumber</td>
+														<td>$error</td>
+														<td>$message</td>
 													</tr>";
               }
               ?>
